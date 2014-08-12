3f0ecbf576b6079573d3ecb9bb68ee738ed7184b
Tom Readings <digitisatom@gmail.com>
2014-08-12 16:38:39 +0100

    example: listing enrolled courses

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-require_once('includes/moodle_queries.php');
 require_once('includes/krumo/class.krumo.php');
 require_once('includes/class.moodlequery.php');
 require_once('config.php');
@@ -16,8 +15,18 @@ echo "<h1>API call to get moodle data</h1>";
 // krumo($student);
 
 $moodle = new MoodleQuery($CFG);
-$student = $moodle->getuser($_COOKIE['MoodleSession']);
+// $student = $moodle->getuser($_COOKIE['MoodleSession']);
+$student = $moodle->getuser(3);
+
+
+$courses = $moodle->getenrolments($student);
+echo '<ul>';
+foreach ($courses as $course) {
+  echo '<li>'.$course['fullname'].'</li>';
+}
+echo '</ul>';
 
 krumo($student); 
+krumo($courses); 
 
 //krumo(student_get_course($student['id']));
